88cf1da51a0611b47a4eb95406c1f67b6865176a
MAIK1362 <miguelarc2204@gmail.com>
2026-03-17 16:37:04 -0600

    Update second_win.py

--- 1994357/second_win.py
+++ 1994357/second_win.py
@@ -1,3 +1,23 @@
 # write a code for the second screen of app
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (QApplication, QWidget,QHBoxLayout, QVBoxLayout,QGroupBox, QRadioButton,QPushButton, QLabel, QListWidget, QLineEdit)
+
+from instr import *
+from second_win import *
+
+
+class MainWin(QWidget):
+    def __init__(self):
+        #la ventana dende esta el saludo
+        super().__init__()
+        #creando y configurando elementos graficos
+        self.initUI()
+        #establece la conexion entre los elementos
+        self.connects()
+        #establece la apariencia de la ventana(etiqueta, tamaño y ubicacion)
+        self.set_appear()
+        #inicio
+        self.show()
+
+
+    
