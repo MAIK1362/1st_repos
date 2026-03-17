ace97ab9bb811c28fa3775cf5b0ebaf142d0741d
MAIK1362 <miguelarc2204@gmail.com>
2026-03-17 16:33:20 -0600

    Update second_win.py

--- 1994357/second_win.py
+++ 1994357/second_win.py
@@ -1,1 +1,3 @@
 # write a code for the second screen of app
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (QApplication, QWidget,QHBoxLayout, QVBoxLayout,QGroupBox, QRadioButton,QPushButton, QLabel, QListWidget, QLineEdit)
